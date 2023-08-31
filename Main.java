cee037ee77ff46d448b2b06f28546f57e41c1830
Kiscica79 <oroszildiko79@gmail.hu>
2023-08-31 17:31:26 +0200

    The second method is ready

--- Main.java
+++ Main.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] tomb = new int[20];
+        Random random = new Random();
+
+        for (int i = 0; i < tomb.length; i++) {
+            tomb[i] = random.nextInt(100) + 1;
+        }
+
+        System.out.println("A tömb elemei:");
+        for (int i = 0; i < tomb.length; i++) {
+            System.out.print(tomb[i] + ", ");
+        }
+        System.out.println();
+
+        int[] tomb2 = {5, 2, 9, 1, 7, 3, 8, 4, 6};
+        int minimum = arrayMinimum(tomb2);
+        System.out.println("A tömb minimuma: " + minimum);
+
+    }
+
+
+    public static int arrayMinimum(int[] tomb) {
+        if (tomb.length == 0) {
+            throw new IllegalArgumentException("A tömb üres!");
+        }
+
+        int minNumber = tomb[0];
+        for (int i = 1; i < tomb.length; i++) {
+            if (tomb[i] < minNumber) {
+                minNumber = tomb[i];
+            }
+        }
+        return minNumber;
+    }
 }
