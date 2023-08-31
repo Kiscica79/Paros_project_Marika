654d1d1f3794be313c2ee1b80925a445d782ecba
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-08-31 18:24:48 +0300

    implement calculateMax method

--- Main.java
+++ Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+package PACKAGE_NAME; public class Main {
+    public static void main(String[] args) {
+        int[] array = {25,2,5,9,7,4,3};
+        System.out.println("The biggest number is " + calculateMax(array));
+
+    }
+
+    public static int calculateMax(int[] array){
+        if(array == null || array.length == 0){
+            throw new IllegalArgumentException("The array must not be null or empty.");
+        }
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+
+        }
+        return max;
+    }
+
 }
